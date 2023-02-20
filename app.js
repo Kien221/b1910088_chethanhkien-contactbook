e24d94b5ed4c23a7c17b15aa3f6c282c4c95476d
kienBT4 <kienBT4@gmail.com>
2023-02-20 16:00:34 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -9,5 +10,11 @@ app.get("/",(req,res)=>{
     res.json({message:"Welcome to contact book application."});
 });
 app.use("/api/contacts",contactsRouter);
+
+app.use((req,res,next)=>{
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
 
 module.exports = app;
